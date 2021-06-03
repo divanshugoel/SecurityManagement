76efd2b3cd92bdb967a4b4681b7dd5e099ce3917
divanshugoel <divanshu.goel339@gmail.com>
2021-06-03 12:27:26 +0530

    inital commit

---
+++ src/test/java/org/webapp/securitymanagement/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package org.webapp.securitymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
